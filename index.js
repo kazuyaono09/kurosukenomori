ef6b97102b752f50378d04c84d1259a742359971
kazuyaono09 <95998168+kazuyaono09@users.noreply.github.com>
2023-02-01 21:49:17 +0900

    ファイルのアップロード
    JSファイルのバージョンアップ、CSSスタイルの新規追加

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector(".humbergar");
 const naviMenu = document.querySelector(".header__navi");  //クリックされた時のメニュー画面
 const menuItems = document.querySelectorAll(".navi__menu__item");
+const sections = document.querySelectorAll(".section");
+
 
 //humbergar--menuをクリックした時の動作
 menuBtn.addEventListener("click" , function() {
@@ -15,3 +17,22 @@ naviMenu.addEventListener("click" , function() {
 this.classList.remove("active__navi");
 menuBtn.classList.toggle("active__humbergar");
 });
+
+window.addEventListener("scroll", () => {
+  animationScroll();
+});
+
+function animationScroll() {
+  const scrollY = window.scrollY;//スクロール量
+  const windowHeight = window.innerHeight;//ウィンドウの高さ
+
+  sections.forEach((section) => {
+    const offsetTop = section.offsetTop;//要素の縦方向の位置。
+    const inviewHeight = 250;
+
+    if(scrollY + windowHeight > offsetTop + inviewHeight) {
+      section.classList.add("section--active");
+    }
+  });
+}
+
