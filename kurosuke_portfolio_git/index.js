d9deb6be0e3012ae91f770b2f765fba900d2812b
kazuyaono09 <95998168+kazuyaono09@users.noreply.github.com>
2023-03-05 18:35:39 +0900

    ファイルのアップロード
    レスポンシブ対応の修正

---
+++ kurosuke_portfolio_git/index.js
@@ -0,0 +1,42 @@
+const menuBtn = document.querySelector(".humbergar");
+const naviMenu = document.querySelector(".header__navi");  //クリックされた時のメニュー画面
+const menuItems = document.querySelectorAll(".navi__menu__item");
+const sections = document.querySelectorAll(".section");
+
+
+//humbergar--menuをクリックした時の動作
+menuBtn.addEventListener("click" , function() {
+  this.classList.toggle("active__humbergar"); //ボタンの切り替え動作
+  naviMenu.classList.toggle("active__navi");
+  menuItems = classList.add("active__item");
+  console.log(menuItems);
+});
+
+//navimenuの背景をクリックした時の動作
+naviMenu.addEventListener("click" , function() {
+this.classList.remove("active__navi");
+menuBtn.classList.toggle("active__humbergar");
+});
+
+window.addEventListener("scroll", () => {
+  animationScroll();
+});
+
+function animationScroll() {
+  const scrollY = window.scrollY;//スクロール量
+  const windowHeight = window.innerHeight;//ウィンドウの高さ
+
+  sections.forEach((section) => {
+    const offsetTop = section.offsetTop;//要素の縦方向の位置。
+    const inviewHeight = 200;
+
+    if(scrollY + windowHeight > offsetTop + inviewHeight) {
+      section.classList.add("section--active");
+    }
+  });
+}
+
+window.addEventListener("load" , function(){
+  // code
+});
+
